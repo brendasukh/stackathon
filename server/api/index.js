8c265ffc3aaf73a1ab107516e46eef03c58ec2af
brendasukh <sukhnyamdorj@Gantsetsegs-MacBook-Air.local>
2017-08-04 15:50:46 -0400

    modified index.js took out database info

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
+// router.use('/users', require('./users'))
 router.use('/image', require('./image'))
 
 router.use((req, res, next) => {
