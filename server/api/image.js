50fe3bc45731d0af1ca308e84f5129d4173a46ed
brendasukh <sukhnyamdorj@Gantsetsegs-MacBook-Air.local>
2017-08-04 22:47:08 -0400

    api accessing data from image

--- server/api/image.js
+++ server/api/image.js
@@ -2,37 +2,98 @@ const router = require('express').Router()
 const fs = require('fs');
 const path = require('path')
 const request = require('request-promise');
-
+var imgur = require('imgur');
+// imgur.setCredentials('brendasukh@gmail.com', 'Brendasukh918');
 
 var index = 1;
-var binaryBuffer, buf
+var binaryBuffer, buf, data
+var link;
+  var options = {
+    method: 'POST',
+    uri:'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+    body: {'url' : 'http://i.imgur.com/7SpSlkC.jpg'},
+    headers: {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '6f7761f334d64fd6858be9240a806eb9' },
+    json: true
+  };
 router.post('/', (req, res, next) => {
   const imageName = 'test' + index + '.jpg';
-  var data= req.body.image.replace(/^data:image\/(png|jpeg);base64,/, "");
+  data= req.body.image.replace(/^data:image\/(png|jpeg);base64,/, "");
   buf = new Buffer(data, 'base64');
-  binaryBuffer = new Buffer(buf, 'binary')
+  binaryBuffer = new Buffer(buf, 'binary');
+  // console.log(data)
+  imgur.uploadBase64(data)
+    .then(function (json) {
+        link = json.data.link;
+        options.body.url=link 
+        // console.log(link)
+        // console.log(json.data.link)
+        return request(options)
+    .then((apiResult) => {
+      console.log((apiResult[0]).scores)
+      return apiResult
+    })
+    .then((apiResult) => {
+
+    })
+    .catch( (err) => {
+      console.log("Api fetch failed", err);
+    });
+    })
+    
+    .catch(function (err) {
+        console.error(err.message)
+    });
+
+
   
   // var binaryBuffer = new Buffer(buf, 'binary')
-  fs.writeFile(path.join(__dirname, `../../public/images/${imageName}`), buf,  (err) => {
-    if(err){
-      res.send('Failure');
-      throw err;
-    }
-    res.send('Success');
+  // fs.writeFile(path.join(__dirname, `../../public/images/${imageName}`), buf,  (err) => {
+  //   if(err){
+  //     res.send('Failure');
+  //     throw err;
+  //   }
+  //   res.send('Success');
+  // })
+  // if (link) {
+  //   console.log(link)
+  // }
+  // var options = {
+  //   method: 'POST',
+  //   uri:'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+  //   body: {'url' : `'${link}'`},
+  //   headers: {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '6f7761f334d64fd6858be9240a806eb9' },
+  //   json: true
+  // };
+
+  // request(options)
+  //   .then((apiResult) => {
+  //     console.log(apiResult)
+  //   })
+  //   .catch( (err) => {
+  //     console.log("Api fetch failed", err);
+  //   });
+    
   })
-})
-// binaryBuffer = fs.readFileSync(path.join(__dirname, '../../public/images/img1.png'))
-// binaryBuffer = fs.createReadStream(p)
-// console.log('binaryBuffer', Object.keys(binaryBuffer))
+// binaryBuffer = fs.readFileSync(path.join(__dirname, '../../public/images/test1.jpg'))
+// // binaryBuffer = fs.createReadStream(p)
+//   buf = new Buffer(binaryBuffer, 'base64');
 // var buffer = new Buffer (binaryBuffer, 'binary')
-//console.log('binary buffer here!', binaryBuffer)
-var options = {
-  method: 'POST',
-  uri:'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
-  body: {url: ''},
-  headers: {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key': 'key goes here' },
-  json: true
-};
+// console.log('binary buffer here!', buf)
+// imgur.uploadBase64(buf)
+//     .then(function (json) {
+//         console.log(json.data.link)
+//     })
+//     .catch(function (err) {
+//         console.error(err.message)
+//     });
+// console.log(link)
+// var options = {
+//   method: 'POST',
+//   uri:'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+//   body: {'url' : `'${link}'`},
+//   headers: {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '6f7761f334d64fd6858be9240a806eb9' },
+//   json: true
+// };
 
 // request(options)
 //   .then((apiResult) => {
