5380027142ed900f13138c676b674fe3c61c199d
vinayarv <vinayarv@users.noreply.github.com>
2017-08-06 12:11:58 -0400

    updated frontend-code

--- client/routes.js
+++ client/routes.js
@@ -4,68 +4,18 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, Camera} from './components'
-import {me} from './store'
+import {Camera} from './components'
 
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount () {
-    this.props.loadInitialData()
-  }
-
-  render () {
-    const {isLoggedIn} = this.props
-
-    return (
+export const Routes = () => {
+  return (
       <Router history={history}>
-        <Main>
-          <Switch>
-            {/* Routes placed here are available to all visitors */}
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-            <Route exact path='/camera' component={Camera} />
-            {
-              isLoggedIn &&
-                <Switch>
-                  {/* Routes placed here are only available after logging in */}
-                  <Route path='/home' component={UserHome} />
-                </Switch>
-            }
-            {/* Displays our Login component as a fallback */}
-            <Route component={Login} />
-          </Switch>
-        </Main>
+        <Route exact path='/' component={Camera} />
       </Router>
     )
-  }
 }
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+export default Routes;
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData () {
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Routes)
-
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
